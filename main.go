d158aa21391243e80ed8e819be57d982b13fd1cb
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-08 16:39:04 -0300

    create first route

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func showStudents(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"id":   1,
+		"nome": "Lucas Mezencio",
+	})
+}
 
 func main() {
 	r := gin.Default()
-	err := r.Run(":5000")
-	if err != nil {
-		return
-	}
+	r.GET("/alunos", showStudents)
+	r.Run(":5000")
 }
