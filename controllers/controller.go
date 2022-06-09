fe72225d7e9c2841ad6e87101997d175efe10ad3
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-09 10:28:41 -0300

    create model aluno

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,15 +1,13 @@
 package controllers
 
 import (
+	"gin-rest-alura/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func ShowAllStudents(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"id":   1,
-		"nome": "Lucas Mezencio",
-	})
+	c.JSON(http.StatusOK, models.Alunos)
 }
 
 func Saudacao(c *gin.Context) {
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "gin-rest-alura/routes"
+import (
+	"gin-rest-alura/models"
+	"gin-rest-alura/routes"
+)
 
 func main() {
+	models.Alunos = []models.Aluno{
+		{
+			Nome: "Lucas Mezencio",
+			CPF:  "12312312312",
+			RG:   "mg12123123",
+		},
+		{
+			Nome: "Segundo Nome",
+			CPF:  "00011122233",
+			RG:   "go12123123",
+		},
+	}
 	routes.HandleRequests()
 }
