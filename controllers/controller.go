0e509cb801b3a929c56543d760b1300e98891b70
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-09 11:40:58 -0300

    fix find aluno by id route - implement not found

--- controllers/controller.go
+++ controllers/controller.go
@@ -36,5 +36,13 @@ func FindStudentById(c *gin.Context) {
 	var aluno models.Aluno
 	id := c.Params.ByName("id")
 	database.DB.First(&aluno, id)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not Found": "aluno nao encontrado",
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, aluno)
 }
